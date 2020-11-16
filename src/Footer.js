ff14f53e2e33be69b03e4f8e09c839201ca80a6b
CHRIS_LEE <chrismlee87@gmail.com>
2020-11-15 23:26:55 -0800

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css';
+
+function Footer() {
+  return (
+      <div className="Footer">
+          <footer>
+            <hr></hr>
+            <p>Chris Lee copyright 2020</p>
+          </footer>
+      </div>
+  )
+}
+
+export default Footer
