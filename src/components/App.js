9a17cfe00be389acc2b0ec22f2850d9ae6c7ce40
CHRIS_LEE <chrismlee87@gmail.com>
2020-11-17 02:04:04 -0800

    master react tutorial complete

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title';
+import POPOSList from './POPOSList';
+import Footer from './Footer.js';
+import { HashRouter as Router, Route } from 'react-router-dom'
+import About from './About'
+import { NavLink } from 'react-router-dom'
+import POPOSDetails from './POPOSDetails'
+
+function App() {
+  return (
+    <Router>
+
+      <div className="App">
+        <Title />
+
+        <Route exact path="/" component={POPOSList}/>
+        <Route path="/about" component={About} />
+        <Route path="/details/:id" component={POPOSDetails} />
+        <Footer />
+
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
