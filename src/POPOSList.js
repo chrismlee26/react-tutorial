c4531c38a5e30986a2c7995682029d8ec1060e5b
CHRIS_LEE <chrismlee87@gmail.com>
2020-11-16 16:00:45 -0800

    dynamic content and footer

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,40 +1,24 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace';
 import './POPOSList.css';
+import data from './sfpopos-data.json'
 
 function POPOSList() {
+
+  const spaces = data.map(( { title, address, images } ) => {
+    return (
+      <POPOSSpace
+        key={title}
+        name={title}
+        address={address}
+        image={images[0]}
+      />
+    )
+  })
+
   return (
     <div className="POPOSList">
-      <POPOSSpace 
-        name="50 California Street"
-        address="50 California St."
-        image="50-california-st.jpg"
-      />
-      <POPOSSpace 
-        name="100 Pine Street"
-        address="100 Pine St."
-        image="100-pine.jpg"
-      />
-        <POPOSSpace 
-        name="101 California Street"
-        address="101 California St."
-        image="101-california.jpg"
-      />
-        <POPOSSpace 
-        name="343 Sansome Roof Garden"
-        address="343 Sansome St."
-        image="343-sansome-roof-garden.jpg"
-      />
-        <POPOSSpace 
-        name="525 Market Street Plaza"
-        address="525 Market Street St."
-        image="525-market-street-plaza.jpg"
-      />
-        <POPOSSpace 
-        name="555 California Street"
-        address="555 California St."
-        image="555-california.jpg"
-      />
+      { spaces }
     </div>
   )
 }
